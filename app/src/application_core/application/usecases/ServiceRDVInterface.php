f7333bbe30ce39139d14e5db9b31c93b790068ba
Robin <robincrtt05@gmail.com>
2025-09-16 14:44:13 +0200

    feat(service/rdv): lister créneaux occupés et consulter rdv

---
+++ app/src/application_core/application/usecases/ServiceRDVInterface.php
@@ -0,0 +1,21 @@
+<?php
+
+namespace toubilib\core\application\usecases;
+
+use toubilib\core\application\dto\CreneauOccupeDTO;
+use toubilib\core\application\dto\RdvDTO;
+
+interface ServiceRDVInterface
+{
+    /**
+     * Liste les créneaux occupés pour un praticien sur une période.
+     * @return CreneauOccupeDTO[]
+     */
+    public function listerCreneauxOccupes(string $praticienId, string $dateDebut, string $dateFin): array;
+
+    /**
+     * Retourne un RDV par son identifiant.
+     */
+    public function consulterRdv(string $id): ?RdvDTO;
+}
+
